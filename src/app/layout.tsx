8e4cee1cbeb53d5b719266b48846639dd098d739
emirmf <134041370+EmirMF@users.noreply.github.com>
2026-04-13 22:22:06 +0700

    update metadata
    Updated metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,10 +18,27 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Pemilu 8EH Radio ITB",
-  description: "Pemilu 8EH Radio ITB 2026",
+  metadataBase: new URL("https://pemilu.8ehradioitb.com"),
+  title: "Pemilu 8EH Radio ITB 2026",
+  description: "Pemilihan Umum 8EH Radio ITB 2026",
   icons: {
-    icon: '/8eh.ico', 
+    icon: '/8eh.ico',
+  },
+  openGraph: {
+    title: "Pemilu 8EH Radio ITB 2026",
+    description: "Pemilu 8EH Radio ITB 2026",
+    url: "https://pemilu.8ehradioitb.com",
+    siteName: "8EH Radio ITB",
+    images: [
+      {
+        url: "https://opengraph.b-cdn.net/production/images/926d8e9c-a7a3-40a9-b8b4-0de2a4a36875.png?token=IKNzn_FUcVlq_Gf8fdp7krktgTVanXpsEyg4_LdRG3E&height=630&width=1200&expires=33288947365",
+        width: 1200,
+        height: 630,
+        alt: "Pemilu 8EH Radio ITB 2026",
+      },
+    ],
+    locale: "id_ID",
+    type: "website",
   },
 };
 
